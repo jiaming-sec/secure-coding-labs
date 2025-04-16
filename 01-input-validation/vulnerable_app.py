f1e67b58eb7fa5984a2f12055ada2c09459dc92a
Jiaming Sec <jiamingqu0728@gmail.com>
2025-04-16 17:04:29 -0400

    Update vulnerable_app.py

--- 01-input-validation/vulnerable_app.py
+++ 01-input-validation/vulnerable_app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-name = request.args.get("name", "")
-return f"<h1>Hello, {name}!</h1>"
+    name = request.args.get("name", "")
+    return f"<h1>Hello, {name}!</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
