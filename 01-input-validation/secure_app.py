99b3f05d031b9d48535cebc9417b1df73f4e62d7
Jiaming Sec <jiamingqu0728@gmail.com>
2025-04-17 16:56:40 -0400

    Update secure_app.py

--- 01-input-validation/secure_app.py
+++ 01-input-validation/secure_app.py
@@ -8,3 +8,6 @@ def index():
     name = request.args.get("name", "")
     safe_name = html.escape(name)
     return f"<h1>Hello, {safe_name}!</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
