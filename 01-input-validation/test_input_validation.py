eeeb8be4dd8350911e60b83862abc9f403630a02
Jiaming Sec <jiamingqu0728@gmail.com>
2025-04-17 17:01:03 -0400

    Update test_input_validation.py

--- 01-input-validation/test_input_validation.py
+++ 01-input-validation/test_input_validation.py
@@ -4,3 +4,4 @@ def test_xss_input():
     test_client = app.test_client()
     malicious_input = "<script>alert('XSS')</script>"
     response = test_client.get(f" /?name={malicious_input}")
+    assert malicious_input not in response.get_data(as_text=True)
