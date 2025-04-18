8c8ead103b8ad0c5de7c3631d8cf88b0b56c263e
Jiaming Sec <jiamingqu0728@gmail.com>
2025-04-18 15:06:18 -0400

    Update secure_auth.py

--- 02-authentication/secure_auth.py
+++ 02-authentication/secure_auth.py
@@ -1,2 +1,5 @@
 from flask import Flask, request, redirect, session, render_template_string
 from werkzeug.security import check_password_hash, generate_password_hash
+
+app = Flask(__name__)
+app.secret_key = "a_very_secure_secret_key"  # Should be stored securely
