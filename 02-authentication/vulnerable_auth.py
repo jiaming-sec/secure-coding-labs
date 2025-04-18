c38f45ee728b8f86daa47cf8dbb2dcb641256b01
Jiaming Sec <jiamingqu0728@gmail.com>
2025-04-18 15:03:33 -0400

    Update vulnerable_auth.py

--- 02-authentication/vulnerable_auth.py
+++ 02-authentication/vulnerable_auth.py
@@ -2,5 +2,5 @@ from flask import Flask, request, redirect, make_response
 
 app = Flask(__name__)
 
-# Hardcoded credentials (bad practic
+# Hardcoded credentials (bad practice)
 users = {"admin": "1234"}
