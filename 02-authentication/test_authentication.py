dbd09e268be66561684e9568b2c1bf465315ca6e
Jiaming Sec <jiamingqu0728@gmail.com>
2025-04-19 23:51:53 -0400

    Update test_authentication.py

--- 02-authentication/test_authentication.py
+++ 02-authentication/test_authentication.py
@@ -4,3 +4,5 @@ def test_login_success():
     client = app.test_client()
     response = client.post("/", data={"username": "admin", "password": "SecureP@ssw0rd!"})
     assert response.status_code == 302  # should redirect on success
+
+def test_login_fail():
