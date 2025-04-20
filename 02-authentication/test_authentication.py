722cd13fc724fb06aa60aac03e3506d36a712994
Jiaming Sec <jiamingqu0728@gmail.com>
2025-04-19 23:51:36 -0400

    Update test_authentication.py

--- 02-authentication/test_authentication.py
+++ 02-authentication/test_authentication.py
@@ -3,3 +3,4 @@ from secure_auth import app
 def test_login_success():
     client = app.test_client()
     response = client.post("/", data={"username": "admin", "password": "SecureP@ssw0rd!"})
+    assert response.status_code == 302  # should redirect on success
