c386b99571d6ee80198d84ff5fd95340e9eae1be
Jiaming Sec <jiamingqu0728@gmail.com>
2025-04-22 23:43:16 -0400

    Update vulnerable_access.py

--- 03-access-control/vulnerable_access.py
+++ 03-access-control/vulnerable_access.py
@@ -1,3 +1,14 @@
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
+users = {
+  "1": {"name": "Alice", "role": "user"},
+  "2": {"name": "Bob", "role": "admin"}
+
+}
+
+users = {
+    "1": {"name": "Alice", "role": "user"},
+    "2": {"name": "Bob", "role": "admin"}
+}
